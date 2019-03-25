0808a3f8da206a78f6d2feac914e67c63976de52
Andy Lin <andylin1997@outlook.com>
2019-03-25 14:33:20 -0700

    initial commit

---
+++ Pokedex/pokedex/src/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div className="header">
+                Pokedex
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ Pokedex/pokedex/src/LoginPage.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import Header from './Header.js'
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+
+export default LoginPage
