0afc32603b0128d7c2aac905d9ab40bcf0de4ec7
etay129 <evangeline129@hotmail.com>
2019-03-25 21:44:49 -0700

    Add personal info data

--- Pokedex/pokedex/routes/html-routes.js
+++ Pokedex/pokedex/routes/html-routes.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 
 module.exports = function(app, connection) {
-  // Example of a mysql query
-  app.get('/towns', function(req, res) {
-    connection.query('SELECT * FROM town', function(err, data) {
+  app.get('/user/:userId', function(req, res) {
+    const userId = req.params.userId;
+    const query = 'SELECT * FROM trainer WHERE id = ' + userId;
+
+    connection.query(query, function(err, data) {
       err ? res.send(err) : res.send(data);
     });
   });
--- Pokedex/pokedex/src/About.js
+++ Pokedex/pokedex/src/About.js
@@ -5,12 +5,29 @@ class About extends Component {
         super(props);
     }
 
+    componentDidMount() {
+      // Temporarily hardcoded until we can pass ID from login page
+      fetch('/user/0')
+        .then(res => res.json())
+        .then(data => this.setState(data[0]));
+    }
+
     render() {
+      if (this.state) {
         return (
-          <div>
-            About
+          <div style={{display:"flex", flexDirection: "row"}}>
+            {this.state.image ? <img style={{marginRight: "20px"}} src={this.state.image} /> : null}
+            <div style={{display: "flex", flexDirection:"column"}}>
+              <div>Name: {this.state.name}</div>
+              <div>Age: {this.state.age || "Unknown"}</div>
+              <div>Gender: {this.state.gender || "Unknown"}</div>
+              <div>Hometown: {this.state.hometown || "Unknown"}</div>
+            </div>
           </div>
         );
+      }
+
+      return null;
     }
 }
 
