98987c5c2c8525dd14eb315346a4ddb937021e44
etay129 <evangeline129@hotmail.com>
2019-03-25 21:08:33 -0700

    first stab at routing

--- Pokedex/pokedex/src/HomePage.js
+++ Pokedex/pokedex/src/HomePage.js
@@ -1,4 +1,14 @@
 import React, {Component} from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import './css/homePage.css'
+
+import About from "./About";
+import Pokemon from "./Pokemon";
 
 class HomePage extends Component {
     constructor(props) {
@@ -6,7 +16,20 @@ class HomePage extends Component {
     }
 
     render() {
-        return <div> yeet </div>;
+        return (
+          <HashRouter>
+            <div>
+              <ul className="menu">
+                <li><NavLink to="/about">Personal Info</NavLink></li>
+                <li><NavLink to="/pokemon">My Pokemon</NavLink></li>
+              </ul>
+              <div className="content">
+                <Route exact path="/about" component={About}/>
+                <Route exact path="/pokemon" component={Pokemon}/>
+              </div>
+            </div>
+          </HashRouter>
+        );
     }
 }
 
---
+++ Pokedex/pokedex/src/Pokemon.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Pokemon extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <div>
+            My Pokemon
+          </div>
+        );
+    }
+}
+
+export default Pokemon
