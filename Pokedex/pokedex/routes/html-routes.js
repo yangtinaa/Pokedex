8241c11455fb3e295a03a4729eb5769ab0d1600f
etay129 <evangeline129@hotmail.com>
2019-03-26 21:21:35 -0700

    Add explore page fricken hell o my

--- Pokedex/pokedex/routes/html-routes.js
+++ Pokedex/pokedex/routes/html-routes.js
@@ -16,8 +16,27 @@ module.exports = function(app, connection) {
     const {name, age, hometown} = body;
     const gender = body.gender == 'unknown' ? null : body.gender;
 
-    const query = 'UPDATE trainer SET name = "' + name + '", age = ' + age + ', gender = "' + gender + '", hometown = "' + hometown + '" WHERE id = ' + userId + ';';
+    const update = 'UPDATE trainer ';
+    const set = 'SET name = "' + name + '", age = ' + age + ', gender = "' + gender + '", hometown = "' + hometown + '" ';
+    const where = 'WHERE id = ' + userId + ';';
+    const query = update + set + where;
 
+    connection.query(query, function(err, data) {
+      err ? res.send(err) : res.send(data);
+    });
+  });
+
+  app.get('/pokemon', function(req, res) {
+    const query = 'SELECT * FROM Pokemon';
+    connection.query(query, function(err, data) {
+      err ? res.send(err) : res.send(data);
+    });
+  });
+
+  app.get('/pokemon/:type', function(req, res) {
+    const type = req.params.type;
+
+    const query = 'SELECT * FROM Pokemon WHERE type = "' + type + '";';
     connection.query(query, function(err, data) {
       err ? res.send(err) : res.send(data);
     });
@@ -25,11 +44,19 @@ module.exports = function(app, connection) {
 
   app.get('/pokemon/:userId', function(req, res) {
     const userId = req.params.userId;
+
     const select = 'SELECT pcb.id, p.name, p.type, p.image, mb.moveName, mb.powerPoint ';
     const from = 'FROM pokemon p, pokemon_CapturedBy pcb, PokemonLearnsMoveA ma, PokemonLearnsMoveB mb ';
     const where = 'WHERE pcb.name = p.name AND pcb.capturedBy = ' + userId + ' AND ma.pokemonID = pcb.id AND mb.moveName = ma.moveName;';
     const query = select + from + where;
 
+    connection.query(query, function(err, data) {
+      err ? res.send(err) : res.send(data);
+    });
+  });
+
+  app.get('/pokemonTypes', function(req, res) {
+    const query = 'SELECT type FROM Pokemon';
     connection.query(query, function(err, data) {
       err ? res.send(err) : res.send(data);
     });
@@ -44,11 +71,39 @@ module.exports = function(app, connection) {
   });
 
   app.get('/gyms', function(req, res) {
-    const querySel = 'SELECT g.gymName, g.badgeName, g.badgeImage, g.townName, t.name ';
-    const queryFrom = 'FROM Gym_LocatedIn_Town g, GymLeader_of_Gym gl, Trainer t '
-    const queryWhere = 'WHERE g.gymName = gl.gymName AND gl.trainerID = t.id;';
+    const select = 'SELECT g.gymName, g.badgeName, g.badgeImage, g.townName, t.name ';
+    const from = 'FROM Gym_LocatedIn_Town g, GymLeader_of_Gym gl, Trainer t '
+    const where = 'WHERE g.gymName = gl.gymName AND gl.trainerID = t.id;';
+    const query = select + from + where;
 
-    connection.query(querySel + queryFrom + queryWhere, function(err, data) {
+    connection.query(query, function(err, data) {
+      err ? res.send(err) : res.send(data);
+    });
+  });
+
+  app.get('/encountered/:userId', function(req, res) {
+    const userId = req.params.userId;
+
+    const select = 'SELECT pokemonName ';
+    const from = 'FROM Trainer_Encounters_Pokemon '
+    const where = 'WHERE trainerID = ' + userId + ';';
+    const query = select + from + where;
+
+    connection.query(query, function(err, data) {
+      err ? res.send(err) : res.send(data);
+    });
+  });
+
+  app.get('/encountered/:userId/:type', function(req, res) {
+    const userId = req.params.userId;
+    const type = req.params.type;
+
+    const select = 'SELECT e.pokemonName ';
+    const from = 'FROM Trainer_Encounters_Pokemon e, Pokemon p '
+    const where = 'WHERE e.trainerID = ' + userId + ' AND e.pokemonName = p.name AND p.type = "' + type + '";';
+    const query = select + from + where;
+
+    connection.query(query, function(err, data) {
       err ? res.send(err) : res.send(data);
     });
   });
--- Pokedex/pokedex/src/Explore.js
+++ Pokedex/pokedex/src/Explore.js
@@ -1,11 +1,130 @@
 import React, {Component} from "react";
-// import './css/trainers.css'
+import './css/pokemon.css'
 
 class Explore extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          encountered: [],
+          allPokemon: [],
+          types: [],
+          filter: "All",
+        };
+    }
+
+    componentDidMount() {
+      // Temporarily hardcoded userId
+      const encounteredReq = fetch('/encountered/0').then(res => res.json());
+      const allPokemonReq = fetch('/pokemon').then(res => res.json());
+      const typeReq = fetch('/pokemonTypes').then(res => res.json());
+
+      Promise.all([encounteredReq,allPokemonReq, typeReq]).then(values =>
+        this._initializeData(values[0], values[1], values[2])
+      )
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.filter !== this.state.filter) {
+        // Temporarily hardcoded userId
+        const encounteredReq =
+          fetch('/encountered/0/' + this.state.filter).then(res => res.json());
+        const allPokemonReq =
+          fetch('/pokemon/' + this.state.filter).then(res => res.json());
+
+        Promise.all([encounteredReq,allPokemonReq]).then(values =>
+          this._updateData(values[0], values[1])
+        )
+      }
+    }
+
+    _initializeData(data, totalData, typesData) {
+      const {encountered, allPokemon} = this._processData(data, totalData);
+      const types = [... new Set(typesData.map(t => t.type))];
+
+      this.setState({encountered, allPokemon, types});
+    }
+
+    _updateData(data, totalData) {
+      const {encountered, allPokemon} = this._processData(data, totalData);
+      this.setState({encountered, allPokemon});
+    }
+
+    _processData(data, totalData) {
+      const encounteredData = data.map(d => d.pokemonName);
+      const encountered = [];
+      const allPokemon = [];
+
+      debugger;
+
+      let i;
+      for(i = 0; i < totalData.length; i++) {
+        encounteredData.includes(totalData[i].name) ?
+          encountered.push(totalData[i]) :
+          allPokemon.push(totalData[i]);
+      }
+
+      return {encountered, allPokemon};
+    }
+
     render() {
       return (
         <div>
-          Explore
+          <div style={{display: "flex", flexDirection: "row", marginBottom: "20px"}}>
+            <div style={{marginRight: "10px"}}>Filters:</div>
+            <select id="type"
+                    name="type"
+                    value={this.state.filter}
+                    onChange={event => this.setState({filter: event.target.value})}>
+              <option value="All">All</option>
+              {this.state.types.map(t => (
+                <option key={t} value={t}>{t}</option>
+              ))}
+            </select>
+          </div>
+          {this.state.encountered.length === 0 ? null :
+            <div>
+              <div style={{marginBottom: "10px"}}>Encountered Pokemon:</div>
+              {this.state.encountered.map(p => (
+                <div key={p.name} className="pokemon-card">
+                  {
+                    p.image ?
+                      <img
+                        className="pokemon-img"
+                        src={p.image}
+                        alt={p.name || "pokemon image"} /> : null
+                  }
+                  <div className="pokemon-info">
+                    <div>Name: {p.name}</div>
+                    <div>Type: {p.type || "Unknown"}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          }
+          {this.state.allPokemon.length === 0 ? null :
+            <div>
+              <div style={{marginBottom: "10px", marginTop: "20px"}}>New Pokemon:</div>
+              {this.state.allPokemon.map(p => (
+                <div key={p.name} style={{position: "relative"}}>
+                  <div className="pokemon-card">
+                    {
+                      p.image ?
+                        <img
+                          className="pokemon-img"
+                          src={p.image}
+                          alt={p.name || "pokemon image"} /> : null
+                    }
+                    <div className="pokemon-info">
+                      <div>Name: {p.name}</div>
+                      <div>Type: {p.type || "Unknown"}</div>
+                    </div>
+                  </div>
+                  <div className="overlay">
+                  </div>
+                </div>
+              ))}
+            </div>
+          }
         </div>
       );
     }
